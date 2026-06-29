637632e4e3d9e4cd8bc0b41d13c998e62b9b4cde
glenrioariesto <glenrioariesto@gmail.com>
2026-06-29 15:58:59 +0700

    Add GitHub Actions deploy and set base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/rute-kurir-roti-pintar/',
     plugins: [react(), tailwindcss()],
     publicDir: 'assets',
     resolve: {
